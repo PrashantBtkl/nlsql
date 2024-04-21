b80d8590ab31a8ab7fd598e971b81582e9d4a2cd
PrashantBtkl <prashant44backup@gmail.com>
2024-04-21 19:17:14 +0530

    refactor: seperate method to generate tables text

--- main.py
+++ main.py
@@ -15,18 +15,21 @@ def main():
     args = parser.parse_args()
 
     conn = psql.Database(args.db_url)
-
-    tables = ""
     results = conn.get_tables()
+    tables = tables_txt(results)
+    prompt = text_to_sql_prompt.generate_prompt(tables, args.question)
+    result = llm.LLM(args.model_path).run_inference(prompt)
+    print(result)
+    conn.disconnect()
+
+def tables_txt(results):
+    tables = ""
     for _, row in enumerate(results):
         tables += f"""
         table: {row[0]}
         columns: {row[1]}
         """
-    prompt = text_to_sql_prompt.generate_prompt(tables, args.question)
-    result = llm.LLM(args.model_path).run_inference(prompt)
-    print(result)
-    conn.disconnect()
+    return tables
 
 if __name__ == "__main__":
     main()
