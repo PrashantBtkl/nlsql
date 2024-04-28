3f4b76a4f4f1af0c13dd489289e521628e27683c
PrashantBtkl <prashant44backup@gmail.com>
2024-04-28 12:46:54 +0530

    feature: groq

---
+++ ai_assistant/groq.py
@@ -0,0 +1,18 @@
+import os
+from groq import Groq
+
+class LLM():
+    def __init__(self):
+        self.client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
+
+    def run_inference(self, prompt: str) -> str:
+        chat_completion = self.client.chat.completions.create(
+        messages=[
+             {
+                 "role": "user",
+                 "content": prompt,
+             }
+        ],
+        model="llama3-70b-8192",
+        )
+        return chat_completion.choices[0].message.content
